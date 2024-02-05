7be0dccc8942ce94eb43d1c10b13142fc90b088b
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-02-05 01:53:15 +0000

    u

--- step-2.cpp
+++ step-2.cpp
@@ -17,7 +17,100 @@
  */
 
 class NBodySimulationMolecularForces : public NBodySimulation {
+  
+  public:
+  void updateBody () {
+    std::cout << "Successfully inherited" << std::endl;
+    timeStepCounter++;
+    maxV   = 0.0;
+    minDx  = std::numeric_limits<double>::max();
+    double c = 0.01;
+    // force0 = force along x direction
+    // force1 = force along y direction
+    // force2 = force along z direction
+    double* force0 = new double[NumberOfBodies];
+    double* force1 = new double[NumberOfBodies];
+    double* force2 = new double[NumberOfBodies];
 
+
+    // Initialise forces to 0 
+    for (int i = 0; i < NumberOfBodies; i++) {
+          force0[i] = 0.0;
+          force1[i] = 0.0;
+          force2[i] = 0.0;
+    }
+
+    const double diameter = 0.2;
+
+    for (int i=0; i<NumberOfBodies; i++) {
+      for (int j = i+1; j < NumberOfBodies; j++) {
+        if (i == j) continue;
+        double f0, f1, f2, dist;
+        f0 = force_calculation(i,j,0);    // force0[0] to [i]
+        f1 = force_calculation(i,j,1);
+        f2 = force_calculation(i,j,2);
+
+        double distance = sqrt(
+                               (x[j][0]-x[i][0]) * (x[j][0]-x[i][0]) +
+                               (x[j][1]-x[i][1]) * (x[j][1]-x[i][1]) +
+                               (x[j][2]-x[i][2]) * (x[j][2]-x[i][2])
+                               );
+
+        // Newton's law
+        force0[i] += f0;
+        force1[i] += f1;
+        force2[i] += f2;
+
+        force0[j] -= f0;
+        force1[j] -= f1;
+        force2[j] -= f2;
+
+        if (distance <= c*(mass[i] + mass[j])){
+
+          // Momentum calculations
+          x[i][0] = (mass[i]*x[i][0] + mass[j]*x[j][0]) / (mass[i]+mass[j]);
+          x[i][1] = (mass[i]*x[i][1] + mass[j]*x[j][1]) / (mass[i]+mass[j]);
+          x[i][2] = (mass[i]*x[i][2] + mass[j]*x[j][2]) / (mass[i]+mass[j]);
+
+          v[i][0] = (mass[i]*v[i][0] + mass[j]*v[j][0]) / (mass[i]+mass[j]);
+          v[i][1] = (mass[i]*v[i][1] + mass[j]*v[j][1]) / (mass[i]+mass[j]);
+          v[i][2] = (mass[i]*v[i][2] + mass[j]*v[j][2]) / (mass[i]+mass[j]);
+          
+          // Mass of merged object
+          mass[i] += mass[j];
+
+          // Remove other merged object from list
+          for (int k = j; k < NumberOfBodies - 1; k++) {
+          x[k] = x[k + 1];
+          v[k] = v[k + 1];
+          mass[k] = mass[k + 1];
+          }
+          NumberOfBodies--;
+          j--;
+        }
+
+      }
+    }
+
+      // x,y,z forces acting on particle 0 => to all particles
+
+    for (int i = 0; i < NumberOfBodies; i++) {
+    x[i][0] = x[i][0] + timeStepSize * v[i][0];
+    x[i][1] = x[i][1] + timeStepSize * v[i][1];
+    x[i][2] = x[i][2] + timeStepSize * v[i][2];
+
+    v[i][0] = v[i][0] + timeStepSize * force0[i] / mass[i];
+    v[i][1] = v[i][1] + timeStepSize * force1[i] / mass[i];
+    v[i][2] = v[i][2] + timeStepSize * force2[i] / mass[i];
+
+    maxV = std::max(std::sqrt(v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2]), maxV);
+    }
+    t += timeStepSize;
+
+    delete[] force0;
+    delete[] force1;
+    delete[] force2;
+  }
 };
 
 /**
