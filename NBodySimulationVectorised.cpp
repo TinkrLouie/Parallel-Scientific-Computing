75a78969b1fc060d50c29dca59e9724d0d0f59dc
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-02-07 23:40:11 +0000

    u

--- NBodySimulationVectorised.cpp
+++ NBodySimulationVectorised.cpp
@@ -83,7 +83,7 @@ class NBodySimulationVectorised : public NBodySimulation {
       }
       //#pragma omp barrier
       dist = sqrt(d[0]*d[0] + d[1]*d[1] + d[2]*d[2]);  // distance between 4th order x to j
-      #pragma omp simd private(dim)
+      #pragma omp simd
       for (dim = 0; dim < 3; dim++) aTemp[3][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 4th order acceleration of i
 
 
