b0566518e9cb70630ca2f5c09897a2ed3aa225d1
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-02-07 12:49:21 +0000

    u

--- NBodySimulationVectorised.cpp
+++ NBodySimulationVectorised.cpp
@@ -25,7 +25,7 @@ class NBodySimulationVectorised : public NBodySimulation {
       if (dist <= (c/NumberOfBodies)*(mass[i] + mass[j])+tolerance){
         // Momentum update
         #pragma omp simd private(dim)
-        for (int dim = 0; dim < 3; dim++) {
+        for (dim = 0; dim < 3; dim++) {
           x[i][dim] = (mass[i]*x[i][dim] + mass[j]*x[j][dim]) / (mass[i]+mass[j]);
           v[i][dim] = (mass[i]*v[i][dim] + mass[j]*v[j][dim]) / (mass[i]+mass[j]);
         }
@@ -38,7 +38,7 @@ class NBodySimulationVectorised : public NBodySimulation {
 
         // Remove other merged object from list
         #pragma omp simd private(dim)
-        for (int dim = 0; dim < 3; dim++) {
+        for (dim = 0; dim < 3; dim++) {
 	      x[j][dim] = x[l][dim];
 	      v[j][dim] = v[l][dim];
         }
@@ -48,12 +48,12 @@ class NBodySimulationVectorised : public NBodySimulation {
       }
 
       #pragma omp simd private(dim)
-      for (int dim = 0; dim < 3; dim++) aTemp[0][dim] = (x[j][dim]-x[i][dim]) * mass[j] / (dist*dist*dist);
+      for (dim = 0; dim < 3; dim++) aTemp[0][dim] = (x[j][dim]-x[i][dim]) * mass[j] / (dist*dist*dist);
       minDx = std::min(minDx,dist);
 
         // Step 2-------------------------------------------------------------
       #pragma omp simd private(dim)
-      for (int dim = 0; dim < 3; dim++) {
+      for (dim = 0; dim < 3; dim++) {
         vTemp[1][dim] = v[i][dim] + aTemp[0][dim]*timeStepSize*0.5;  // compute 2nd order v
         xTemp[1][dim] = x[i][dim] + v[i][dim]*timeStepSize*0.5; // compute 2nd order x
         d[dim] = x[j][dim] - xTemp[1][dim];  // compute dx,dy,dz of 2nd order x
@@ -61,11 +61,11 @@ class NBodySimulationVectorised : public NBodySimulation {
       //#pragma omp barrier
       dist = sqrt(d[0]*d[0] + d[1]*d[1] + d[2]*d[2]);  // distance between 2nd order x to j
       #pragma omp simd private(dim)
-      for (int dim = 0; dim < 3; dim++) aTemp[1][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 2nd order acceleration of i
+      for (dim = 0; dim < 3; dim++) aTemp[1][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 2nd order acceleration of i
 
       // Step 3-------------------------------------------------------------
       #pragma omp simd private(dim)
-      for (int dim = 0; dim < 3; dim++) {
+      for (dim = 0; dim < 3; dim++) {
         vTemp[2][dim] = v[i][dim] + aTemp[1][dim]*timeStepSize*0.5;  // compute 3rd order v
         xTemp[2][dim] = x[i][dim] + vTemp[1][dim]*timeStepSize*0.5; // compute 3rd order x
         d[dim] = x[j][dim] - xTemp[2][dim];  // compute dx,dy,dz of 3rd order x
@@ -73,11 +73,11 @@ class NBodySimulationVectorised : public NBodySimulation {
       //#pragma omp barrier
       dist = sqrt(d[0]*d[0] + d[1]*d[1] + d[2]*d[2]);  // distance between 3rd order x to j
       #pragma omp simd private(dim)
-      for (int dim = 0; dim < 3; dim++) aTemp[2][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 3rd order acceleration of i
+      for (dim = 0; dim < 3; dim++) aTemp[2][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 3rd order acceleration of i
 
       // Step 4-------------------------------------------------------------
       #pragma omp simd private(dim)
-      for (int dim = 0; dim < 3; dim++) {
+      for (dim = 0; dim < 3; dim++) {
         vTemp[3][dim] = v[i][dim] + aTemp[2][dim]*timeStepSize*0.5;  // compute 4th order v
         xTemp[3][dim] = x[i][dim] + vTemp[2][dim]*timeStepSize*0.5; // compute 4th order x
         d[dim] = x[j][dim] -  xTemp[3][dim];  // compute dx,dy,dz of 4th order x
@@ -85,13 +85,13 @@ class NBodySimulationVectorised : public NBodySimulation {
       //#pragma omp barrier
       dist = sqrt(d[0]*d[0] + d[1]*d[1] + d[2]*d[2]);  // distance between 4th order x to j
       #pragma omp simd private(dim)
-      for (int dim = 0; dim < 3; dim++) aTemp[3][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 4th order acceleration of i
+      for (dim = 0; dim < 3; dim++) aTemp[3][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 4th order acceleration of i
 
 
       // Update x and v-----------------------------------------------------
       //#pragma omp barrier
       #pragma omp for simd private(dim)
-      for (int dim = 0; dim < 3; dim++) {
+      for (dim = 0; dim < 3; dim++) {
         x[i][dim] = x[i][dim] + nr*(v[i][dim] + 2*vTemp[1][dim] + 2*vTemp[2][dim] + vTemp[3][dim]) * timeStepSize;
         v[i][dim] = v[i][dim] + nr*(aTemp[0][dim] + 2*aTemp[1][dim] + 2*aTemp[2][dim] + aTemp[3][dim]) * timeStepSize;
       }
