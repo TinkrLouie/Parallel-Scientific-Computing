8fd5262dfb357c4ecb4281fa94896b4bd4a1a78f
HaCH <69476769+TinkrLouie@users.noreply.github.com>
2024-02-07 12:29:24 +0000

    u

--- NBodySimulationVectorised.cpp
+++ NBodySimulationVectorised.cpp
@@ -5,82 +5,110 @@
 
 class NBodySimulationVectorised : public NBodySimulation {
   public:
-  //#pragma omp declare simd
-  double force_calculation (int i, int j, int direction){
-    // Euclidean distance
-    const double distance = sqrt(
-                                 (x[j][0]-x[i][0]) * (x[j][0]-x[i][0]) +
-                                 (x[j][1]-x[i][1]) * (x[j][1]-x[i][1]) +
-                                 (x[j][2]-x[i][2]) * (x[j][2]-x[i][2])
-                                 );
-    const double distance3 = distance * distance * distance;
-    minDx = std::min( minDx,distance );
+    // Runge-Kutta 4th Order
+    void rk4 (int i, int j) {
+      double xTemp[4][3];
+      double vTemp[4][3];
+      double aTemp[4][3];
+      double d[3];
+      double dist, nr = 1.0/6;
+      double c = 1e-2;
+      double tolerance = 0.05;      // tweak tolerance here
+      
+      // Step 1-------------------------------------------------------------
+      dist = sqrt((x[j][0]-x[i][0]) * (x[j][0]-x[i][0]) +
+                  (x[j][1]-x[i][1]) * (x[j][1]-x[i][1]) +
+                  (x[j][2]-x[i][2]) * (x[j][2]-x[i][2])
+                 );
+      // Collision detection
+      if (dist <= (c/NumberOfBodies)*(mass[i] + mass[j])+tolerance){
+        // Momentum update
+        #pragma omp simd private(dim)
+        for (int dim = 0; dim < 3; dim++) {
+          x[i][dim] = (mass[i]*x[i][dim] + mass[j]*x[j][dim]) / (mass[i]+mass[j]);
+          v[i][dim] = (mass[i]*v[i][dim] + mass[j]*v[j][dim]) / (mass[i]+mass[j]);
+        }
 
-    return (x[j][direction]-x[i][direction]) * mass[i]*mass[j] / distance3;
-  }
+        // Mass of merged object
+        mass[i] += mass[j];
 
-  void updateBody () {
-    timeStepCounter++;
-    maxV   = 0.0;
-    minDx  = std::numeric_limits<double>::max();
+        // Decrement n bodies
+        const int l = --NumberOfBodies;
 
-    // force0 = force along x direction
-    // force1 = force along y direction
-    // force2 = force along z direction
-    double* force0 = new double[NumberOfBodies];
-    double* force1 = new double[NumberOfBodies];
-    double* force2 = new double[NumberOfBodies];
+        // Remove other merged object from list
+        #pragma omp simd private(dim)
+        for (int dim = 0; dim < 3; dim++) {
+	      x[j][dim] = x[l][dim];
+	      v[j][dim] = v[l][dim];
+        }
+        mass[j] = mass[l];
+        j--;
+        return;
+      }
+
+      #pragma omp simd private(dim)
+      for (int dim = 0; dim < 3; dim++) aTemp[0][dim] = (x[j][dim]-x[i][dim]) * mass[j] / (dist*dist*dist);
+      minDx = std::min(minDx,dist);
+
+        // Step 2-------------------------------------------------------------
+      #pragma omp simd private(dim)
+      for (int dim = 0; dim < 3; dim++) {
+        vTemp[1][dim] = v[i][dim] + aTemp[0][dim]*timeStepSize*0.5;  // compute 2nd order v
+        xTemp[1][dim] = x[i][dim] + v[i][dim]*timeStepSize*0.5; // compute 2nd order x
+        d[dim] = x[j][dim] - xTemp[1][dim];  // compute dx,dy,dz of 2nd order x
+      }
+      //#pragma omp barrier
+      dist = sqrt(d[0]*d[0] + d[1]*d[1] + d[2]*d[2]);  // distance between 2nd order x to j
+      #pragma omp simd private(dim)
+      for (int dim = 0; dim < 3; dim++) aTemp[1][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 2nd order acceleration of i
+
+      // Step 3-------------------------------------------------------------
+      #pragma omp simd private(dim)
+      for (int dim = 0; dim < 3; dim++) {
+        vTemp[2][dim] = v[i][dim] + aTemp[1][dim]*timeStepSize*0.5;  // compute 3rd order v
+        xTemp[2][dim] = x[i][dim] + vTemp[1][dim]*timeStepSize*0.5; // compute 3rd order x
+        d[dim] = x[j][dim] - xTemp[2][dim];  // compute dx,dy,dz of 3rd order x
+      }
+      //#pragma omp barrier
+      dist = sqrt(d[0]*d[0] + d[1]*d[1] + d[2]*d[2]);  // distance between 3rd order x to j
+      #pragma omp simd private(dim)
+      for (int dim = 0; dim < 3; dim++) aTemp[2][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 3rd order acceleration of i
+
+      // Step 4-------------------------------------------------------------
+      #pragma omp simd private(dim)
+      for (int dim = 0; dim < 3; dim++) {
+        vTemp[3][dim] = v[i][dim] + aTemp[2][dim]*timeStepSize*0.5;  // compute 4th order v
+        xTemp[3][dim] = x[i][dim] + vTemp[2][dim]*timeStepSize*0.5; // compute 4th order x
+        d[dim] = x[j][dim] -  xTemp[3][dim];  // compute dx,dy,dz of 4th order x
+      }
+      //#pragma omp barrier
+      dist = sqrt(d[0]*d[0] + d[1]*d[1] + d[2]*d[2]);  // distance between 4th order x to j
+      #pragma omp simd private(dim)
+      for (int dim = 0; dim < 3; dim++) aTemp[3][dim] = d[dim]*mass[j]/(dist*dist*dist);  // 4th order acceleration of i
 
 
-    // Initialise forces to 0 
-    for (int i = 0; i < NumberOfBodies; i++) {
-          force0[i] = 0.0;
-          force1[i] = 0.0;
-          force2[i] = 0.0;
-    }
-
-
-    //#pragma omp parallel for 
-    for (int i=0; i<NumberOfBodies; i++) {
-      //#pragma omp simd 
-      for (int j = i+1; j < NumberOfBodies; j++) {
-        double f0, f1, f2;
-        f0 = force_calculation(i,j,0);    // Calculate force between i and j
-        f1 = force_calculation(i,j,1);
-        f2 = force_calculation(i,j,2);
-
-        // Force acting i by j
-        force0[i] += f0;
-        force1[i] += f1;
-        force2[i] += f2;
-        // Force acting on j by i, opposite magnitude hence negative sign
-        force0[j] -= f0;
-        force1[j] -= f1;
-        force2[j] -= f2;
+      // Update x and v-----------------------------------------------------
+      //#pragma omp barrier
+      #pragma omp for simd private(dim)
+      for (int dim = 0; dim < 3; dim++) {
+        x[i][dim] = x[i][dim] + nr*(v[i][dim] + 2*vTemp[1][dim] + 2*vTemp[2][dim] + vTemp[3][dim]) * timeStepSize;
+        v[i][dim] = v[i][dim] + nr*(aTemp[0][dim] + 2*aTemp[1][dim] + 2*aTemp[2][dim] + aTemp[3][dim]) * timeStepSize;
       }
     }
 
-    // Update velocity and position  
-    //#pragma omp simd reduction(max:maxV)
-    for (int i = 0; i < NumberOfBodies; i++) {
-      
-      x[i][0] += timeStepSize *v[i][0];
-      x[i][1] += timeStepSize *v[i][1];
-      x[i][2] += timeStepSize *v[i][2];
-  
-      v[i][0] += timeStepSize * force0[i] / mass[i];
-      v[i][1] += timeStepSize * force1[i] / mass[i];
-      v[i][2] += timeStepSize * force2[i] / mass[i];
-      
-      maxV = std::max(std::sqrt(v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2]), maxV);
+    void updateBody () {
+      timeStepCounter++;
+      maxV   = 0.0;
+      minDx  = std::numeric_limits<double>::max();
+      for (int i=0; i<NumberOfBodies; i++) {
+            // Possible vectorisation and parallism on inner loop
+        for (int j = 0; j < NumberOfBodies; j++) {
+          // Calculate position and velocity by performing RK4 on i and j
+          if (i==j) continue;
+          rk4(i,j);
+          maxV = std::max(std::sqrt(v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2]), maxV);
+        }
+      }
+      t += timeStepSize;
     }
-
-
-    
-    t += timeStepSize;
-
-    delete[] force0;
-    delete[] force1;
-    delete[] force2;
-  }
 };
